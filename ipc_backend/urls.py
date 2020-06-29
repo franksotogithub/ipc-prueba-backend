ba4ef934b6b17295e73008cfc05cc34d2325fefa
franksotogithub <franksoto2012@gmail.com>
2020-06-29 00:41:10 -0500

    seg

--- ipc_backend/urls.py
+++ ipc_backend/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import  static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     url(r'api/formatos/',include('formatos.api.urls',namespace='api_formatos')),
     url(r'api/shared/',include('shared.api.urls',namespace='api_shared'))
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
